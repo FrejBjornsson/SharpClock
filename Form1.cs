087ab4c13e55f9ca2c14b1156177d3fadad86eb2
Frej <50577711+FrejBjornsson@users.noreply.github.com>
2020-10-15 18:04:07 +0200

    Added via upload. Windows Forms App.
    Added Clock in a Windows Forms App. Just a simple digital clock.

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpClock
+{
+    public partial class ClockForm : Form
+    {
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            Clock.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void clock_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clock_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
